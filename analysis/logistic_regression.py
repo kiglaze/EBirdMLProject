8c014a175185ed1198e6366e0ba12d94c9b0a8fe
kiglaze <kiglaze@ncsu.edu>
2025-11-05 17:45:41 -0500

    making sure logistic regression creation file is up to date

--- analysis/logistic_regression.py
+++ analysis/logistic_regression.py
@@ -1,14 +1,21 @@
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
+import os
 
 def main():
-    df = pd.read_csv("../data_preprocessing/restricted_geo_loc_sighting_weekly/osprey_glacier_bay_weekly.csv")
+    osprey_glacier_bay_df = pd.read_csv("../data_preprocessing/restricted_geo_loc_sighting_weekly/osprey_glacier_bay_weekly.csv")
+    generate_cyclical_log_reg_plots(osprey_glacier_bay_df, "Osprey", "Glacier Bay, Alaska")
 
-    # scale weeks to [0,1] for interpretability (optional)
-    df['WEEK_FRACTION'] = (df['WEEK_IN_YEAR'] - 1) / 52.0
+    ca_condor_gc_df = pd.read_csv("../data_preprocessing/restricted_geo_loc_sighting_weekly/ca_condor_grand_canyon_weekly.csv")
+    generate_cyclical_log_reg_plots(ca_condor_gc_df, "California Condor", "Grand Canyon")
 
+    alt_puffin_ma_coast_df = pd.read_csv("../data_preprocessing/restricted_geo_loc_sighting_weekly/atlantic_puffin_ma_coastal_weekly.csv")
+    generate_cyclical_log_reg_plots(alt_puffin_ma_coast_df, "Atlantic Puffin", "Massachusetts Coastal Area")
+
+def generate_cyclical_log_reg_plots(df: DataFrame, species_name, location_name):
     df['WEEK_SIN'] = np.sin(2 * np.pi * df['WEEK_IN_YEAR'] / 52)
     df['WEEK_COS'] = np.cos(2 * np.pi * df['WEEK_IN_YEAR'] / 52)
 
@@ -31,11 +38,18 @@ def main():
     plt.scatter(df['WEEK_IN_YEAR'], y, alpha=0.2, label='Observations')
     plt.plot(week_grid, pred_probs, label='Cyclic Logistic Regression', color='red')
     plt.xlabel('Week In Year')
-    plt.ylabel('Probability of Osprey Presence')
-    plt.title('Glacier Bay, Alaska (2015-2025): Osprey Presence vs. Week of Year (Cyclic Logistic Regression)')
+    plt.ylabel(f"Probability of {species_name} Presence")
+    plt.title(f"{location_name} (2015-2025): {species_name} Presence vs. Week of Year (Cyclic Logistic Regression)")
     plt.legend()
     plt.tight_layout()
+
+    output_directory = "logistic_regression_plots"
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    output_filename = f"{output_directory}/log_reg_{species_name.replace(' ', '_').lower()}_{location_name.replace(' ', '_').lower()}.png"
+    plt.savefig(output_filename)
     plt.show()
+    plt.close()
 
 if __name__ == "__main__":
     main()
